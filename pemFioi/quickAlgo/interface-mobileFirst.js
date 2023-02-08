cf7803fe9b5b22c5faaafb09e6f74da88b517b0b
Hannah42 <rauterberg@mailbox.org>
2023-02-08 16:41:05 +0100

    change appheight

--- pemFioi/quickAlgo/interface-mobileFirst.js
+++ pemFioi/quickAlgo/interface-mobileFirst.js
@@ -1379,7 +1379,7 @@ $(document).ready(function() {
 
     const appHeight = () => {
         const doc = document.documentElement
-        doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+        doc.style.setProperty('--app-height', `${window.innerHeight - 100}px`)
     }
     window.addEventListener('resize', appHeight)
     appHeight()
