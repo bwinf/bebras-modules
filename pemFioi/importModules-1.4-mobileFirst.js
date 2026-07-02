bd4dede39ce08191aedb5c4df166f0aa5e9af0fc
Hannah42 <rauterberg@mailbox.org>
2026-07-02 11:38:09 +0200

    Add jwinf specific design for codecast

--- pemFioi/importModules-1.4-mobileFirst.js
+++ pemFioi/importModules-1.4-mobileFirst.js
@@ -99,6 +99,7 @@
          'blockly-turtle': {src: modulesPath+"/pemFioi/blocklyTurtle_lib.js", id: "blocklyTurtle_lib"},
          'blockly-processing': {src: modulesPath+"/pemFioi/blocklyProcessing_lib.js", id: "blocklyProcessing_lib"},
          'jwinf_css': {type: "stylesheet", src: modulesPath+"/pemFioi/jwinf.css", id: "jwinf_css"}, // for BWINF
+         'jwinf-codecast_css': {type: "stylesheet", src: modulesPath + "/pemFioi/jwinf-codecast.css",id: "jwinf-codecast_css"}, // for BWINF
          'blockly-ia-1.0': {src: modulesPath+"/pemFioi/blocklyIA_lib-1.0.js", id: "blocklyIA_lib-1.0"},
 
          'quickAlgo_utils': {src: modulesPath+"/pemFioi/quickAlgo/utils.js", id: "quickAlgo_utils"},
